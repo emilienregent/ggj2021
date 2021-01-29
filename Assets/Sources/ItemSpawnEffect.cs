758e9e022afacb7eb0c1fec8a0ad7c202b4982a5
MonsieurTweek <gaetan.regent@gmail.com>
2021-01-29 22:45:10 +0100

    PhysicMaterial on items + conic spawn

--- Assets/Sources/ItemSpawnEffect.cs
+++ Assets/Sources/ItemSpawnEffect.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class ItemSpawnEffect : MonoBehaviour
 {
 
+    public int RotationAngle = 45;
     public float RotationForce = 1000f;
     public float SideForce = 250f;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Rotation
+        transform.rotation = Quaternion.Euler(0, Random.Range(-1 * RotationAngle, RotationAngle), 0);
 
+        // Force
         Rigidbody body = GetComponent<Rigidbody>();
-
         body.AddForce(-1 * transform.right * SideForce);
         body.AddTorque(0f, 0f, Random.Range(RotationForce/2, RotationForce));
 
