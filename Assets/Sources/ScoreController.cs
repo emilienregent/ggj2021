68b7c6f20c110d37ab8094599ccf44a1dde4ac41
emilienregent <emilien.regent@gmail.com>
2021-01-29 16:27:30 +0100

    Add in-game canvas with score

---
+++ Assets/Sources/ScoreController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    public TMP_Text label = null;
+
+    private int currentScore = 0;
+
+    public void SetScore(int score)
+    {
+        currentScore = score;
+
+        label.text = currentScore.ToString();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(FakeUpdateScore());
+    }
+
+    private IEnumerator FakeUpdateScore()
+    {
+        yield return new WaitForSeconds(Random.Range(1f, 3f));
+
+        SetScore(currentScore + Random.Range(5, 10));
+
+        StartCoroutine(FakeUpdateScore());
+    }
+}
