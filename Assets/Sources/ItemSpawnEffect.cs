3794c0a7ebf683a6517a3d9d5ab8fb6945c1e5b7
MonsieurTweek <gaetan.regent@gmail.com>
2021-01-29 17:30:29 +0100

    Change items to prefab variants + wip on ItemSpawnEffect

---
+++ Assets/Sources/ItemSpawnEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawnEffect : MonoBehaviour
+{
+
+    public float UpForce = 1f;
+    public float SideForce = .1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        float xForce = Random.Range(-SideForce, SideForce);
+        float yForce = Random.Range(UpForce / 2f, UpForce);
+        float zForce = Random.Range(-SideForce, SideForce);
+
+        Vector3 force = new Vector3(xForce, yForce, zForce);
+
+        GetComponent<Rigidbody>().velocity = force;
+
+    }
+}
---
+++ Assets/Sources/ItemSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+
+    public List<Item> AvailableItems = new List<Item>();
+
+    private List<Item> _pool = new List<Item>();
+
+    public List<Item> Pool { get => _pool; set => _pool = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
