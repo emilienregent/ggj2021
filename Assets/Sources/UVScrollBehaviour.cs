f01016bc92dbdb874c8ed9780feb056a9932fe7a
emilienregent <emilien.regent@gmail.com>
2021-01-29 15:59:12 +0100

    Animate conveyor and create prefabs for scene management

---
+++ Assets/Sources/UVScrollBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class UVScrollBehaviour : MonoBehaviour
+{
+    public MeshRenderer meshRenderer = null;
+    public Vector2 speed = Vector2.zero;
+    private Vector2 offset = Vector2.zero;
+
+    private void Update()
+    {
+        offset += speed;
+
+        meshRenderer.material.SetTextureOffset("_MainTex", offset);
+    }
+}
