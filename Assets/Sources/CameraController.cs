d051bf592452e35f675e13d2669cd42574997969
emilienregent <emilien.regent@gmail.com>
2021-01-30 22:52:18 +0100

    Camera intro with cinemachine

---
+++ Assets/Sources/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public CinemachineVirtualCamera introCamera = null;
+    public CinemachineVirtualCamera gameCamera = null;
+
+    private void Start()
+    {
+        introCamera.Priority = 0;
+        gameCamera.Priority = 100;
+    }
+}
