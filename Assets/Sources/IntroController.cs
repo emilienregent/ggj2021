fdee3e6b165dc062eff1387c14233e7abfccbae7
emilienregent <emilien.regent@gmail.com>
2021-01-31 01:23:58 +0100

    Update intro with call to action

--- Assets/Sources/IntroController.cs
+++ Assets/Sources/IntroController.cs
@@ -1,9 +1,24 @@
-﻿using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class IntroController : MonoBehaviour
 {
     public GameObject text = null;
+
+    public GameObject door = null;
+    public LeanTweenType doorEaseType = LeanTweenType.linear;
+
+    [Header("Button")]
+    public GameObject button = null;
+    public LeanTweenType buttonEaseType = LeanTweenType.linear;
+    public Color normalColor = Color.white;
+    public Color emissiveColor = Color.white;
+
+    private Material buttonMaterial = null;
+
+    private void Start()
+    {
+        buttonMaterial = button.GetComponent<MeshRenderer>().material;
+    }
 
     private void Update()
     {
@@ -19,13 +34,24 @@ public class IntroController : MonoBehaviour
                 }
             }
         }
+
+        if (GameManager.instance.CurrentGameState == GameState.Intro)
+        {
+            buttonMaterial.SetColor("_OutlineColor", Color.Lerp(normalColor, emissiveColor, Mathf.PingPong(Time.time, 1)));
+        }
+        else if(button.gameObject.activeSelf)
+        {
+            buttonMaterial.SetColor("_OutlineColor", Color.black);
+        }
     }
 
     private void OnButtonClicked()
     {
         GameManager.instance.CurrentGameState = GameState.Tutorial;
 
-        LeanTween.moveZ(gameObject, transform.position.z + 0.5f, 0.5f).setEase(LeanTweenType.easeInBounce);
+        LeanTween.moveZ(button, button.transform.position.z + 0.3f, 0.5f).setEase(buttonEaseType);
+        LeanTween.rotateY(door, 90f, 1f).setEase(doorEaseType);
+
         text.SetActive(false);
     }
 }
