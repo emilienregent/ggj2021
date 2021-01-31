34d729155086de99e569ec91ad3681229f45dda9
emilienregent <emilien.regent@gmail.com>
2021-01-31 02:17:11 +0100

    Improve call to action on intro

--- Assets/Sources/IntroController.cs
+++ Assets/Sources/IntroController.cs
@@ -13,11 +13,22 @@ public class IntroController : MonoBehaviour
     public Color normalColor = Color.white;
     public Color emissiveColor = Color.white;
 
-    private Material buttonMaterial = null;
+    private float _timeStart = 0f;
+    private Material _buttonMaterial = null;
+
+    private int _tweenId = -1;
 
     private void Start()
     {
-        buttonMaterial = button.GetComponent<MeshRenderer>().material;
+        _timeStart = Time.time;
+        _buttonMaterial = button.GetComponent<MeshRenderer>().material;
+
+        _tweenId = LeanTween.rotateAround(button, button.transform.up, -15f, 0.1f).setOnComplete(OnStartCallToAction).setDelay(15f).id;
+    }
+
+    private void OnStartCallToAction()
+    {
+        _tweenId = LeanTween.rotateAround(button, button.transform.up, 30f, 0.1f).setLoopPingPong(-1).id;
     }
 
     private void Update()
@@ -37,11 +48,13 @@ public class IntroController : MonoBehaviour
 
         if (GameManager.instance.CurrentGameState == GameState.Intro)
         {
-            buttonMaterial.SetColor("_OutlineColor", Color.Lerp(normalColor, emissiveColor, Mathf.PingPong(Time.time, 1)));
+            _buttonMaterial.SetColor("_OutlineColor", Color.Lerp(normalColor, emissiveColor, Mathf.PingPong(Time.time, 0.5f)));
+            _buttonMaterial.SetFloat("_OutlineWidth", Mathf.Lerp(0.02f, 0.09f, Mathf.PingPong(Time.time, 0.5f)));
         }
         else if(button.gameObject.activeSelf)
         {
-            buttonMaterial.SetColor("_OutlineColor", Color.black);
+            _buttonMaterial.SetColor("_OutlineColor", Color.black);
+            _buttonMaterial.SetFloat("_OutlineWidth", 0.07f);
         }
     }
 
@@ -51,6 +64,11 @@ public class IntroController : MonoBehaviour
 
         LeanTween.moveZ(button, button.transform.position.z + 0.3f, 0.5f).setEase(buttonEaseType);
         LeanTween.rotateY(door, 90f, 1f).setEase(doorEaseType);
+
+        if (LeanTween.isTweening(_tweenId))
+        {
+            LeanTween.cancel(_tweenId);
+        }
 
         text.SetActive(false);
     }
